3238049b865850e7270a47fa01b689f2f23a460b
Daffa Yusuf Mahendra <daffayusuf075@gmail.com>
2026-06-10 13:31:27 +0700

    add dashboard staff

---
+++ resources/views/components/dashboard.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+    <h1>Dashboard Staff (Sedang dikerjakan oleh tim Frontend)</h1>
+</body>
+
+</html>
